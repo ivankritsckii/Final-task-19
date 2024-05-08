f7c706fb7fa8712d376bd4c07f35c94a90d712df
Dmitry Andriyanov <facklak@mail.ru>
2024-05-08 16:29:21 +0300

    feat: add window.addEventListener in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello");
+import { apiInitialization } from "./apiRequests/apiInitialization";
+import { apiGetProducts } from "./apiRequests/apiGetProducts";
+import { createMain } from "./pages/main/createMain";
+import { loading } from "./modules/loading/loading";
+//import { registrationPage } from "./pages/registration/registrationPage";
+
+window.addEventListener("load", async () => {
+  const body = document.querySelector("body");
+  if (body) {
+    const connect = apiInitialization();
+    createMain(body);
+    loading();
+    // show products
+    connect.then(async () => {
+      apiGetProducts().then(() => {
+        loading();
+      });
+    });
+
+    // show registration form
+    /*
+    connect.then(async () => {
+      const content = document.querySelector(".content") as HTMLElement;
+      if (content) {
+        registrationPage(content);
+      }
+      loading();
+    });
+    */
+  }
+});
