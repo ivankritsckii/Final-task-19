c6c751cce27145397201fde4206cf736a537d5fd
Dmitry Andriyanov <facklak@mail.ru>
2024-05-08 15:26:50 +0300

    feat: create apiGetProducts function

---
+++ src/apiRequests/apiGetProducts.ts
@@ -0,0 +1,28 @@
+import { createProductsPage } from "../helpers/creators/createProductsPage";
+
+export async function apiGetProducts() {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append(
+    "Authorization",
+    `${sessionStorage.getItem("token-type")} ${sessionStorage.getItem("token")}`,
+  );
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow" as const,
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.us-central1.gcp.commercetools.com/rsschool-asdaasd/products?limit=100",
+      requestOptions,
+    );
+
+    const result = await response.text();
+    createProductsPage(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
