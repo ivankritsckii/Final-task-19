a977e067cbad29ae872be1295f7aa8a3b751e1a0
Dmitry Andriyanov <facklak@mail.ru>
2024-05-10 13:49:02 +0300

    fix: rewrite index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
 import { apiInitialization } from "./apiRequests/apiInitialization";
-//import { apiGetProducts } from "./apiRequests/apiGetProducts";
 import { createMain } from "./pages/main/createMain";
 import { loading } from "./modules/loading/loading";
-import { registrationPage } from "./pages/registration/registrationPage";
+import { route } from "./router/route";
+import { createHeader } from "./modules/header/header";
+import { showProductByUrl } from "./pages/main/content/showProductByUrl";
+import { isProductPage } from "./helpers/checks/isProductPage";
 const styles = require("./pages/style.module.scss");
 
 window.addEventListener("load", async () => {
+  window.onhashchange = (event) => {
+    loading();
+    const change = route(event.newURL);
+    change.then(async () => {
+      loading();
+    });
+  };
+
   const body = document.querySelector("body");
   if (body) {
     body?.classList.add(styles.body);
     const connect = apiInitialization();
+    createHeader(body);
     createMain(body);
-    loading();
-    // show products
-    /*
-    connect.then(async () => {
-      apiGetProducts().then(() => {
-        loading();
-      });
-    });
-    */
 
-    // show registration form
-    connect.then(async () => {
-      const content = document.querySelector(".content") as HTMLElement;
-      if (content) {
-        registrationPage(content);
+    connect.then(() => {
+      if (isProductPage()) {
+        showProductByUrl(window.location.hash.replace("#", ""));
+      } else {
+        route(window.location.hash);
       }
-      loading();
     });
   }
 });
