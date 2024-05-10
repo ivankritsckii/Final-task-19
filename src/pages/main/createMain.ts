5ca6f6359d209c158a0dc65c1211369020453341
Dmitry Andriyanov <facklak@mail.ru>
2024-05-10 13:37:12 +0300

    fix: rewrite createMain function

--- src/pages/main/createMain.ts
+++ src/pages/main/createMain.ts
@@ -1,5 +1,6 @@
 import { createElement } from "../../helpers/creators/createElement";
 import { createLoading } from "../../modules/loading/loading";
+import { emailAlreadyPopup } from "../registration/popup/emailAlreadyPopup";
 const styles = require("./content/content.module.scss");
 
 export function createMain(parent: HTMLBodyElement): void {
@@ -8,4 +9,5 @@ export function createMain(parent: HTMLBodyElement): void {
   createLoading(main);
   main.append(content);
   parent.append(main);
+  emailAlreadyPopup(parent);
 }
