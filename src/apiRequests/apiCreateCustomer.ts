ce842a8bd0ad2a8cc1e753c9951cc3dda93228c9
Dmitry Andriyanov <facklak@mail.ru>
2024-05-11 13:29:48 +0300

    fix: rewrite apiCreateCustomer function

--- src/apiRequests/apiCreateCustomer.ts
+++ src/apiRequests/apiCreateCustomer.ts
@@ -1,16 +1,18 @@
-export function apiCreateCustomer(
+export async function apiCreateCustomer(
   emailInput: HTMLInputElement,
   nameInput: HTMLInputElement,
   surnameInput: HTMLInputElement,
   passwordInput: HTMLInputElement,
-): void {
-  console.log("apiCreateCustomer");
+) {
   const email = emailInput.value;
   const name = nameInput.value;
   const surname = surnameInput.value;
   const password = passwordInput.value;
   const token = sessionStorage.getItem("token");
   const tokenType = sessionStorage.getItem("token-type");
+
+  const birthInput = document.getElementById("birth") as HTMLInputElement;
+  const birth = birthInput.value;
 
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -21,6 +23,7 @@ export function apiCreateCustomer(
     firstName: name,
     lastName: surname,
     password: password,
+    dateOfBirth: birth,
   });
 
   const requestOptions = {
@@ -30,25 +33,27 @@ export function apiCreateCustomer(
     redirect: "follow" as const,
   };
 
-  fetch(
-    "https://api.us-central1.gcp.commercetools.com/rsschool-asdaasd/customers",
-    requestOptions,
-  )
-    .then((response) => {
-      console.log(response);
-      if (response.status === 400) {
-        console.log("EMAIL");
-        const popup = document.querySelector(".popup");
-        const popupButton = document.querySelector(
-          ".popup__button",
-        ) as HTMLButtonElement;
-        popup?.classList.add("popup_active");
-        popupButton.focus();
-      }
-      console.log(response.text());
-    })
-    //.then((result) => console.log(result))
-    .catch((error) => {
-      console.error("error: ", error);
-    });
+  try {
+    const response = await fetch(
+      "https://api.us-central1.gcp.commercetools.com/rsschool-asdaasd/customers",
+      requestOptions,
+    );
+
+    if (response.status === 400) {
+      const popup = document.querySelector(".popup");
+      const popupButton = document.querySelector(
+        ".popup__button",
+      ) as HTMLButtonElement;
+      popup?.classList.add("popup_active");
+      popupButton.focus();
+    }
+
+    if (response.status === 201) {
+      const result = await response.text();
+      const json = JSON.parse(result);
+      return json;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
