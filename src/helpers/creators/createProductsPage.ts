dc971119c4a2862957e14210aae80b6e0458cc6e
Dmitry Andriyanov <facklak@mail.ru>
2024-05-11 13:39:52 +0300

    feat: create createSelectCountry function

--- src/helpers/creators/createProductsPage.ts
+++ src/helpers/creators/createProductsPage.ts
@@ -3,13 +3,11 @@ import { apiGetProducts } from "../../apiRequests/apiGetProducts";
 import { createContent } from "../creators/productCard/createContent";
 
 export async function createProductsPage(): Promise<void> {
-  console.log("CREATE MAIN");
   const content = document.querySelector(".content") as HTMLElement;
   content.innerHTML = "";
   const products = await apiGetProducts();
   if (products) {
     const data = JSON.parse(products as string).results;
-    console.log("data", data);
     data.forEach((element: Result) => {
       if (content) {
         createContent(content, element);
