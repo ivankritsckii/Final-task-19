a1b24c26934591983f79f1d55ad3063ab06f9140
Dmitry Andriyanov <facklak@mail.ru>
2024-05-11 21:04:40 +0300

    feat: create addWatchFromInput function

--- src/pages/registration/registrationPage.ts
+++ src/pages/registration/registrationPage.ts
@@ -1,7 +1,9 @@
 import { createRegistrationForm } from "../../helpers/creators/registration/createRegistrationForm";
 import { createFormHandlers } from "../../helpers/creators/registration/createFormHandlers";
+import { addWatchFromInput } from "../../helpers/creators/registration/addWatchFromInput";
 
 export function registrationPage(parent: HTMLElement): void {
   createRegistrationForm(parent);
   createFormHandlers();
+  addWatchFromInput();
 }
