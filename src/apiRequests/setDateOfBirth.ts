24d96520a94c6d9e4e5467d1a2678a709a269df1
Dmitry Andriyanov <facklak@mail.ru>
2024-05-11 13:33:16 +0300

    feat: create setDateOfBirth function

---
+++ src/apiRequests/setDateOfBirth.ts
@@ -0,0 +1,35 @@
+export async function setDateOfBirth(id: string, value: string) {
+  const myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    `${sessionStorage.getItem("token-type")} ${sessionStorage.getItem("token")}`,
+  );
+
+  const raw = JSON.stringify({
+    version: 1,
+    actions: [
+      {
+        action: "setDateOfBirth",
+        dateOfBirth: `${value}`,
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow" as const,
+  };
+
+  try {
+    const response = await fetch(
+      `https://api.us-central1.gcp.commercetools.com/rsschool-asdaasd/customers/${id}`,
+      requestOptions,
+    );
+    const result = await response.text();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
