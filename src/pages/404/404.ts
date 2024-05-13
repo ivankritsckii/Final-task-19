69206e43c43898f7676f298513b3b902070b9b9e
Dmitry Andriyanov <facklak@mail.ru>
2024-05-13 21:47:06 +0300

    feat: create normal 404page

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,9 +1,39 @@
-//TODO: эту функцию нужно доделать
+import { createElement } from "../../helpers/creators/createElement";
+import { createImage } from "../../helpers/creators/createImage";
+import { createNavigationNotPage } from "../../helpers/creators/createNavigationNotPage";
+const styles = require("./404.module.scss");
+
 export function notPage() {
-  console.log("NOT PAGE");
-  const content = document.getElementById("content");
-  if (content) {
-    content.innerHTML = "";
-    content.textContent = "404 PAGE";
-  }
+  const content = document.getElementById("content") as HTMLDivElement;
+  content.innerHTML = "";
+
+  const container = createElement("div", styles["notPage-container"]);
+  const title = createElement(
+    "h1",
+    styles["notPage-title"],
+    "This page does not exist",
+  );
+  const description = createElement(
+    "p",
+    styles["notPage-description"],
+    "No signal here! We can't find this page that you are looking for.",
+  );
+  const descriptionNav = createElement(
+    "p",
+    styles["notPage-description"],
+    "Please follow the links below:",
+  );
+
+  const navigation = createNavigationNotPage();
+
+  const image = createImage(
+    styles["notPage-image"],
+    "https://cs11.pikabu.ru/post_img/2020/03/17/4/1584421566125394075.gif",
+    "404 image",
+    500,
+    480,
+  );
+
+  container.append(title, description, descriptionNav, navigation, image);
+  content.append(container);
 }
