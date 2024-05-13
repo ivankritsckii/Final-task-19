e39d9b7123521fdef6a744dd4db429631e8e597d
Dmitry Andriyanov <facklak@mail.ru>
2024-05-13 15:42:11 +0300

    delete: watchPressing.ts

--- src/helpers/watchPressing.ts
+++
@@ -1,9 +0,0 @@
-export function watchPressing(
-  inputIN: HTMLInputElement,
-  inputOut: HTMLInputElement,
-): void {
-  const active = document.getElementById("one-address") as HTMLInputElement;
-  if (active.checked) {
-    inputOut.value = inputIN.value;
-  }
-}
