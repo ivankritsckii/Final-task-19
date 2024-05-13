fce3c89db6950d23ffe2dde13a435bf86a94f6ae
Dmitry Andriyanov <facklak@mail.ru>
2024-05-13 15:44:20 +0300

    fix: rewrite registrationPage function

--- src/pages/registration/registrationPage.ts
+++ src/pages/registration/registrationPage.ts
@@ -1,9 +1,9 @@
 import { createRegistrationForm } from "../../helpers/creators/registration/createRegistrationForm";
 import { createFormHandlers } from "../../helpers/creators/registration/createFormHandlers";
-import { addWatchFromInput } from "../../helpers/creators/registration/addWatchFromInput";
+import { changeStateBilling } from "../../helpers/creators/registration/changeStateBilling";
 
 export function registrationPage(parent: HTMLElement): void {
   createRegistrationForm(parent);
   createFormHandlers();
-  addWatchFromInput();
+  changeStateBilling();
 }
