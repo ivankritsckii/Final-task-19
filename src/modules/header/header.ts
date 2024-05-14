8abb2270f2791fee840272ae0ac28c08471c371a
Dmitry Andriyanov <facklak@mail.ru>
2024-05-14 17:50:10 +0300

    feat: added normal header with links

--- src/modules/header/header.ts
+++ src/modules/header/header.ts
@@ -1,22 +1,63 @@
+import { headerBurger } from "./headerBurger/headerBurger";
 import { createElement } from "../../helpers/creators/createElement";
 import { createLink } from "../../helpers/creators/createLink";
 import { route } from "../../router/route";
+const styles = require("./header.module.scss");
 
-//TODO: эту фукцию ТОЧНО нужно переписать))
-export function createHeader(parrent: HTMLElement) {
-  const header = createElement("header", "header");
+export function createHeader(parrent: HTMLElement): void {
+  const header = createElement("header", styles.header);
   const nav = createElement("nav", "nav");
-  const ul = createElement("ul", "nav__list");
-  const li = createElement("li", "nav__item");
-  const link = createLink("nav__link", "#registration", false);
-  link.textContent = "registration";
-  link.addEventListener("click", (event: Event) => {
+  const list = createElement("ul", "nav__list");
+
+  const itemMain = createElement("li", "nav__item");
+  const linkMain = createLink("nav__link", "", false);
+  itemMain.append(linkMain);
+  linkMain.textContent = "Main";
+  linkMain.addEventListener("click", (event: Event) => {
     event.preventDefault();
-    route(link.href);
+    route(linkMain.href);
   });
-  li.append(link);
-  ul.append(li);
-  nav.append(ul);
+
+  const itemAboutUs = createElement("li", "nav__item");
+  const linkAboutUs = createLink("nav__link", "#aboutUs", false);
+  itemAboutUs.append(linkAboutUs);
+  linkAboutUs.textContent = "About us";
+  linkAboutUs.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    route(linkAboutUs.href);
+  });
+
+  const itemRegistration = createElement("li", "nav__item");
+  const linkRegistration = createLink("nav__link", "#registration", false);
+  itemRegistration.append(linkRegistration);
+  linkRegistration.textContent = "Registration";
+  linkRegistration.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    route(linkRegistration.href);
+  });
+
+  const itemLogin = createElement("li", "nav__item");
+  const linkLogin = createLink("nav__link", "#login", false);
+  itemLogin.append(linkLogin);
+  linkLogin.textContent = "Login";
+  linkLogin.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    route(linkLogin.href);
+  });
+
+  const itemLogout = createElement("li", "nav__item");
+  itemLogout.classList.add("nav__item_disable");
+  const linkLogout = createLink("nav__link", "#logout", false);
+  itemLogout.append(linkLogout);
+  linkLogout.textContent = "Logout";
+  linkLogout.addEventListener("click", (event: Event) => {
+    event.preventDefault();
+    route(linkLogout.href);
+  });
+
+  list.append(itemMain, itemAboutUs, itemRegistration, itemLogin, itemLogout);
+  nav.append(list);
   header.append(nav);
   parrent.append(header);
+  headerBurger(header);
 }
