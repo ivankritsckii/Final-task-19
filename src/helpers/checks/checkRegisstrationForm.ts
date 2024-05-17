cfd0f7d06ffc43648cbc40dc1f2497176aea5154
Dmitry Andriyanov <facklak@mail.ru>
2024-05-17 16:45:05 +0300

    fix: delete console logs and commented checkRegistrationForm for the future

--- src/helpers/checks/checkRegisstrationForm.ts
+++ src/helpers/checks/checkRegisstrationForm.ts
@@ -1,4 +1,4 @@
-export function checkRegisstrationForm(
+/*export function checkRegisstrationForm(
   emailInput: HTMLInputElement,
   nameInput: HTMLInputElement,
   surnameInput: HTMLInputElement,
@@ -8,25 +8,17 @@ export function checkRegisstrationForm(
   const namePattern = /^[A-Z][a-zA-Z]{2,}$/;
   const passwordPattern = /[0-9a-fA-F]{6,8}/;
   if (emailPattern.test(emailInput.value)) {
-    console.log("Email валидный");
   } else {
-    console.log("Email невалидный");
   }
   if (namePattern.test(nameInput.value)) {
-    console.log("name валидный");
   } else {
-    console.log("name невалидный");
   }
   if (namePattern.test(surnameInput.value)) {
-    console.log("surname валидный");
   } else {
-    console.log("surname невалидный");
   }
   if (passwordPattern.test(passwordInput.value)) {
-    console.log("password валидный");
     return true;
   } else {
-    console.log("password невалидный");
   }
   return false;
-}
+}*/
--- src/helpers/creators/createProductsPage.ts
+++ src/helpers/creators/createProductsPage.ts
@@ -4,9 +4,8 @@ import { createContent } from "../creators/productCard/createContent";
 export function createProductsPage(request: string): void {
   const content = document.querySelector(".content") as HTMLElement;
   const data = JSON.parse(request).results;
-  console.log("data", data);
+
   data.forEach((element: Result) => {
-    console.log("element: ", element);
     if (content) {
       createContent(content, element);
     }
