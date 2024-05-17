6b3ea2741d19eaa08393802ed2c4ada71a2f62e1
Dmitry Andriyanov <facklak@mail.ru>
2024-05-17 16:52:09 +0300

    fix: deleted console log

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,6 +1,5 @@
 //TODO: эту функцию нужно доделать
 export function notPage() {
-  console.log("NOT PAGE");
   const content = document.getElementById("content");
   if (content) {
     content.innerHTML = "";
