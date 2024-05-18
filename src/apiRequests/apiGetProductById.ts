927c6bf1b87de22f2be1fc0059c839f91139b355
Dmitry Andriyanov <53373055+AmIln@users.noreply.github.com>
2024-05-18 09:05:42 +0300

    Api requests (#9)
    * feat: create apiCreateCustomer function
    
    * feat: create apiGetProductById function
    
    * feat: create apiGetProducts function
    
    * feat: create apiInitialization function
    
    * fix: delete console logs

---
+++ src/apiRequests/apiGetProductById.ts
@@ -0,0 +1,28 @@
+import { Result } from "../helpers/interfaces/Results";
+
+export async function apiGetProductById(idProduct: string) {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append(
+    "Authorization",
+    `${sessionStorage.getItem("token-type")} ${sessionStorage.getItem("token")}`,
+  );
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow" as const,
+  };
+
+  try {
+    const response = await fetch(
+      `https://api.us-central1.gcp.commercetools.com/rsschool-asdaasd/products/${idProduct}`,
+      requestOptions,
+    );
+    const result = await response.text();
+    const json = JSON.parse(result) as Result;
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
