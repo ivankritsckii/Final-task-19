0a130ebb045b381109dfb897b2cf332af457c15a
Dmitry Andriyanov <facklak@mail.ru>
2024-05-18 18:12:00 +0300

    feat: removed comments, placed arguments inside the function

--- src/apiRequests/apiAuthCustomer.ts
+++ src/apiRequests/apiAuthCustomer.ts
@@ -1,12 +1,14 @@
 import { createNotification } from "../notification/createNotificationElem";
 import { loginUser } from "./apiloginCustomer";
 
-export const apiAuthorizeUser = async (email: string, password: string) => {
+export const apiAuthorizeUser = async () => {
   const authHost = "https://auth.us-central1.gcp.commercetools.com";
   const projectKey = "rsschool-asdaasd";
   const clientId = "FaK8q3E8G33isxu_Nq_LerOh";
   const clientSecret = "u9mXZFVZZDRvP4i10vNB5zm3FSQJ9uDl";
-  // const scope = "manage_customers";
+  const loginForm = document.querySelector(".login-form") as HTMLFormElement;
+  const emailInput = loginForm.querySelector('input[type="email"]') as HTMLInputElement;
+  const passwordInput = loginForm.querySelector('input[placeholder="Password"]') as HTMLInputElement;
 
   const url = `${authHost}/oauth/${projectKey}/customers/token`;
 
@@ -17,9 +19,8 @@ export const apiAuthorizeUser = async (email: string, password: string) => {
 
   const body = new URLSearchParams({
     grant_type: "password",
-    username: email,
-    password: password,
-    // scope: scope
+    username: emailInput.value,
+    password: passwordInput.value,
   });
 
   const requestOptions = {
@@ -37,10 +38,9 @@ export const apiAuthorizeUser = async (email: string, password: string) => {
     const result = await response.json();
     localStorage.setItem("access_token", result.access_token);
     localStorage.setItem("refresh_token", result.refresh_token);
-    await loginUser(result.access_token, email, password);
+    await loginUser(result.access_token, emailInput.value, passwordInput.value);
     return result;
   } catch (error) {
-    // console.error("Request failed", error);
     createNotification("error", "Authentication failed. Verify your Email and Password and  try again.");
   }
 };
