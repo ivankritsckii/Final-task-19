66d7062baaeb3d652903edfa85c6a80f7d34bcc2
Dmitry Andriyanov <facklak@mail.ru>
2024-05-29 22:32:45 +0300

    fix: upgreat changeImformation function

--- src/helpers/checks/changeImformation.ts
+++ src/helpers/checks/changeImformation.ts
@@ -1,15 +1,19 @@
 import { changeEmail } from "../change/changeEmail";
+import { changeName } from "../change/changeName";
+import { changeSurname } from "../change/changeSurname";
+import { changeBirth } from "../change/changeBirth";
 
 export function changeImformation(idInput: string): void {
   const input = document.getElementById(idInput) as HTMLInputElement;
+  console.log(idInput);
   //TODO: добавить другие команды к инпутам
   const comands = {
-    inform__email: changeEmail(input.value),
-    inform__name: "name",
-    inform__surname: "surname",
-    inform__birth: "birth",
-    inform__password: "password",
+    inform__email: () => changeEmail(input.value),
+    inform__name: () => changeName(input.value),
+    inform__lastName: () => changeSurname(input.value),
+    inform__birth: () => changeBirth(input.value),
+    inform__password: () => "changePassword(input.value)",
   } as unknown as { [key: string]: () => boolean };
   console.log(comands[idInput]);
-  comands[idInput];
+  comands[idInput] && comands[idInput]();
 }
