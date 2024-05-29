55f83ba40157c8631eeb90ca81c149621debe2e8
Dmitry Andriyanov <facklak@mail.ru>
2024-05-29 20:53:36 +0300

    feat: create Address interface

---
+++ src/helpers/interfaces/Address.ts
@@ -0,0 +1,12 @@
+export interface Address {
+  apartment: string;
+  building: string;
+  city: string;
+  country: string;
+  email: string;
+  firstName: string;
+  id: string;
+  lastName: string;
+  postalCode: string;
+  streetName: string;
+}
