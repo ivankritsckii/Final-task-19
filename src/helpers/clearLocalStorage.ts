803240fddd81247ce81a3f0c55206aec2cc5f50a
Dmitry Andriyanov <facklak@mail.ru>
2024-05-29 20:52:22 +0300

    feat: added clear customerId

--- src/helpers/clearLocalStorage.ts
+++ src/helpers/clearLocalStorage.ts
@@ -2,4 +2,5 @@ export function clearLocalStorage(): void {
   // localStorage.clear();
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
+  localStorage.removeItem("customerId");
 }
