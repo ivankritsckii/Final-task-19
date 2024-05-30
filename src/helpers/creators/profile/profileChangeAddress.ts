97bf89c7e6f972406fa88d95f37caa20b2de479b
Dmitry Andriyanov <facklak@mail.ru>
2024-05-30 14:19:41 +0300

    feat: added addEventListener to save button

--- src/helpers/creators/profile/profileChangeAddress.ts
+++ src/helpers/creators/profile/profileChangeAddress.ts
@@ -1,9 +1,11 @@
+import { changeAddress } from "../../change/changeAddress";
 import { createElement } from "../createElement";
 import { createInput } from "../createInput";
 import { activateInput } from "./activateInput";
 
 export function profileChangeAddress(
-  nameInput: string,
+  addressId: string,
+  wrapperClass: string,
   city: string,
   postcode: string,
   street: string,
@@ -11,55 +13,26 @@ export function profileChangeAddress(
   apartment: string,
 ): HTMLElement {
   const wrapper = createElement("div", "profile-inform");
-  wrapper.classList.add("profile-inform_address");
+  wrapper.classList.add("profile-inform_address", wrapperClass);
+  wrapper.setAttribute("name", addressId);
 
-  const cityInput = createInput(
-    "text",
-    "profile-inform__input",
-    `inform__city-${nameInput}`,
-    `inform__city-${nameInput}`,
-    false,
-  );
+  const cityInput = createInput("text", "profile-inform__input", "inform__city", "", false);
   cityInput.value = city;
   cityInput.disabled = true;
 
-  const postcodeInput = createInput(
-    "number",
-    "profile-inform__input",
-    `inform__postcode-${nameInput}`,
-    `inform__postcode-${nameInput}`,
-    false,
-  );
+  const postcodeInput = createInput("number", "profile-inform__input", "inform__postcode", "", false);
   postcodeInput.value = postcode;
   postcodeInput.disabled = true;
 
-  const streetInput = createInput(
-    "text",
-    "profile-inform__input",
-    `inform__postcode-${nameInput}`,
-    `inform__postcode-${nameInput}`,
-    false,
-  );
+  const streetInput = createInput("text", "profile-inform__input", "inform__street", "", false);
   streetInput.value = street;
   streetInput.disabled = true;
 
-  const houseInput = createInput(
-    "text",
-    "profile-inform__input",
-    `inform__house-${nameInput}`,
-    `inform__house-${nameInput}`,
-    false,
-  );
+  const houseInput = createInput("text", "profile-inform__input", "inform__house", "", false);
   houseInput.value = house;
   houseInput.disabled = true;
 
-  const apartmentInput = createInput(
-    "number",
-    "profile-inform__input",
-    `inform__apartment-${nameInput}`,
-    `inform__apartment-${nameInput}`,
-    false,
-  );
+  const apartmentInput = createInput("number", "profile-inform__input", "inform__apartment", "", false);
   apartmentInput.value = apartment;
   apartmentInput.disabled = true;
 
@@ -68,10 +41,15 @@ export function profileChangeAddress(
   const editButton = createElement("div", "profile-inform__edit", "edit");
   editButton.classList.add("profile-inform-address__edit");
   editButton.addEventListener("click", () => {
+    saveButton.classList.toggle("profile-inform-address__save_disable");
     activateInput(buttonsBlock);
   });
   const saveButton = createElement("div", "profile-inform__save", "save");
   saveButton.classList.add("profile-inform-address__save", "profile-inform-address__save_disable");
+  saveButton.addEventListener("click", () => {
+    if (saveButton.classList.contains("profile-inform-address__save_disable")) return;
+    changeAddress(wrapper);
+  });
   buttonsBlock.append(editButton, saveButton);
 
   wrapper.append(cityInput, postcodeInput, streetInput, houseInput, apartmentInput, buttonsBlock);
