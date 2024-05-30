8f6ec0b43e47ea28690e2cd64348a7f392802a35
Dmitry Andriyanov <facklak@mail.ru>
2024-05-30 12:01:03 +0300

    feat: added changePassword to changeImformation

--- src/helpers/checks/changeImformation.ts
+++ src/helpers/checks/changeImformation.ts
@@ -2,6 +2,7 @@ import { changeEmail } from "../change/changeEmail";
 import { changeName } from "../change/changeName";
 import { changeSurname } from "../change/changeSurname";
 import { changeBirth } from "../change/changeBirth";
+import { changePassword } from "../change/changePassword";
 
 export function changeImformation(idInput: string): void {
   const input = document.getElementById(idInput) as HTMLInputElement;
@@ -12,7 +13,7 @@ export function changeImformation(idInput: string): void {
     inform__name: () => changeName(input.value),
     inform__lastName: () => changeSurname(input.value),
     inform__birth: () => changeBirth(input.value),
-    inform__password: () => "changePassword(input.value)",
+    inform__password: () => changePassword(input.value),
   } as unknown as { [key: string]: () => boolean };
   console.log(comands[idInput]);
   comands[idInput] && comands[idInput]();
