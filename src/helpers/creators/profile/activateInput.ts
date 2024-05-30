d0894d64be14f057d9063645729d858b6a0d881b
Dmitry Andriyanov <facklak@mail.ru>
2024-05-30 17:50:25 +0300

    feat: active/deactive to select country

--- src/helpers/creators/profile/activateInput.ts
+++ src/helpers/creators/profile/activateInput.ts
@@ -1,7 +1,12 @@
 export function activateInput(input: HTMLElement): void {
   const inputs = input.parentNode?.querySelectorAll("input");
-  console.log(inputs);
+  const selects = input.parentNode?.querySelectorAll("select");
+
   inputs?.forEach((input) => {
     input.disabled ? (input.disabled = false) : (input.disabled = true);
+  });
+
+  selects?.forEach((select) => {
+    select.disabled ? (select.disabled = false) : (select.disabled = true);
   });
 }
