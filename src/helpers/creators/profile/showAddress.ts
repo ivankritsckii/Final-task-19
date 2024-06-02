13c9a3a0757fcae6b2b18aae9f063d98c5d23f1a
Dmitry Andriyanov <facklak@mail.ru>
2024-06-02 20:40:58 +0300

    feat: added TODO

--- src/helpers/creators/profile/showAddress.ts
+++ src/helpers/creators/profile/showAddress.ts
@@ -1,7 +1,7 @@
 export function showAddress(): void {
   const select = document.getElementById("select-address") as HTMLSelectElement;
   const addresses = document.querySelectorAll(".profile-inform_address");
-
+  //TODO: проверить работу, когда адресов много
   addresses.forEach((element) => {
     element.classList.add("profile-inform_disable");
   });
