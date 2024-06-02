e41109a266065d81eb66137107d447aeace087db
Dmitry Andriyanov <facklak@mail.ru>
2024-06-02 19:43:36 +0300

    feat: rewrite profileChangeBlock function

--- src/helpers/creators/profile/profileChangeBlock.ts
+++ src/helpers/creators/profile/profileChangeBlock.ts
@@ -5,7 +5,11 @@ import { activateInput } from "./activateInput";
 
 const styles = require("./profileChange.module.scss");
 
-export function profileChangeBlock(typeInput: string, nameInput: string, contentInput: string): HTMLElement {
+export async function profileChangeBlock(
+  typeInput: string,
+  nameInput: string,
+  contentInput: string,
+): Promise<HTMLElement> {
   const wrapper = createElement("div", styles["profile-inform"]);
 
   const currentInput = createInput(typeInput, "profile-inform__input", nameInput, nameInput, false);
@@ -17,15 +21,17 @@ export function profileChangeBlock(typeInput: string, nameInput: string, content
   const editButton = createElement("div", "profile-inform__edit", "edit");
   editButton.addEventListener("click", () => {
     if (editButton.classList.contains("disable_btn")) return;
+
     activateInput(currentInput);
     saveButton.classList.remove("profile-inform__save_disable");
     editButton.classList.add("disable_btn");
   });
+
   const saveButton = createElement("div", "profile-inform__save", "save");
   saveButton.classList.add("profile-inform__save_disable");
   saveButton.addEventListener("click", () => {
     if (saveButton.classList.contains("profile-inform__save_disable")) return;
-    console.log(currentInput.id);
+
     activateInput(currentInput);
     changeImformation(currentInput.id);
     saveButton.classList.add("profile-inform__save_disable");
