ca30384c8751cd83dfbed333807e3ce10e3833c9
Dmitry Andriyanov <facklak@mail.ru>
2024-06-03 17:58:49 +0300

    fix: error  lack of addresses

--- src/helpers/creators/profile/showAddress.ts
+++ src/helpers/creators/profile/showAddress.ts
@@ -1,6 +1,7 @@
 export function showAddress(): void {
   const select = document.getElementById("select-address") as HTMLSelectElement;
   const addresses = document.querySelectorAll(".profile-inform_address");
+  if (addresses.length < 1) return;
   //TODO: проверить работу, когда адресов много
   addresses.forEach((element) => {
     element.classList.add("profile-inform_disable");
