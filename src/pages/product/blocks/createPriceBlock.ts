51d9fc1b6d50ebcfd7b704d156f3ecdf9825958a
Dmitry Andriyanov <facklak@mail.ru>
2024-06-04 21:00:25 +0300

    feat: added button delete, and addEventListener to add/delete product

--- src/pages/product/blocks/createPriceBlock.ts
+++ src/pages/product/blocks/createPriceBlock.ts
@@ -1,6 +1,11 @@
 import { Result } from "../../../helpers/interfaces/Results";
 import { createElement } from "../../../helpers/creators/createElement";
 import { getProductDiscountById } from "../../../apiRequests/getProductDiscountById";
+import { apiCreateShoppingList } from "../../../apiRequests/shoppingList/apiCreateShoppingList";
+import { apiAddProductToShoppingList } from "../../../apiRequests/shoppingList/apiAddProductToShoppingList";
+import { getCustomerById } from "../../../apiRequests/getCustomerById";
+import { apiGetShoppingListByKey } from "../../../apiRequests/shoppingList/apiGetShoppingListByKey";
+import { apiDeleteProductToShoppingList } from "../../../apiRequests/shoppingList/apiDeleteProductToShoppingList";
 
 export async function createPriceBlock(product: Result): Promise<HTMLElement> {
   const current = product.masterData.current;
@@ -8,8 +13,25 @@ export async function createPriceBlock(product: Result): Promise<HTMLElement> {
   const priceBlock = createElement("div", "price-block");
   const pricesText = createElement("div", "prices");
   const price = createElement("span", "price", String(current.masterVariant.prices[0].value.centAmount / 100) + "$");
-  // const buttonSend = createElement("button", "product__button", "SEND");
   const buttonSend = createElement("button", "login-btn-grad", "Add");
+  buttonSend.addEventListener("click", async () => {
+    const user = localStorage.getItem("customerId");
+    if (!user) return;
+    const customer = await getCustomerById(user);
+    await apiCreateShoppingList(customer.id);
+    await apiAddProductToShoppingList(product.id);
+
+    console.log("add product: ", await apiGetShoppingListByKey(customer.firstName));
+  });
+
+  const buttonDelete = createElement("button", "login-btn-grad", "Delete");
+  buttonDelete.addEventListener("click", async () => {
+    const user = localStorage.getItem("customerId");
+    if (!user) return;
+    const customer = await getCustomerById(user);
+    await apiDeleteProductToShoppingList(product.id);
+    console.log("after delete: ", await apiGetShoppingListByKey(customer.firstName));
+  });
 
   if (current.masterVariant.prices[0].discounted) {
     const discoundId = current.masterVariant.prices[0].discounted.discount.id;
@@ -28,7 +50,7 @@ export async function createPriceBlock(product: Result): Promise<HTMLElement> {
   } else {
     pricesText.append(price);
   }
-  priceBlock.append(pricesText, buttonSend);
+  priceBlock.append(pricesText, buttonSend, buttonDelete);
 
   return priceBlock;
 }
