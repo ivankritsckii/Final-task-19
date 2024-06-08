4ca1e1f01f191f6a906c500e4dd67af4a76163ca
Dmitry Andriyanov <facklak@mail.ru>
2024-06-08 20:58:09 +0300

    fix: function checkFilters moved to redrawProducts

--- src/helpers/creators/productCard/createContent.ts
+++ src/helpers/creators/productCard/createContent.ts
@@ -1,11 +1,7 @@
 import { Result } from "../../interfaces/Results";
 import { createCard } from "./createCard";
-import { checkFilters } from "../../checks/checkFilters";
 
 export async function createContent(parent: HTMLElement, element: Result): Promise<void> {
-  if (!checkFilters(element)) {
-    return;
-  }
   const cardProduct = await createCard(element);
   parent.append(cardProduct);
 }
