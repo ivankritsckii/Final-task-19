e0324919d04f72b04876723db9ded7a4a0e03202
Dmitry Andriyanov <facklak@mail.ru>
2024-06-09 13:56:59 +0300

    fix: added basketKey in localStorage

--- src/apiRequests/apiloginCustomer.ts
+++ src/apiRequests/apiloginCustomer.ts
@@ -31,6 +31,7 @@ export const loginUser = async (accessToken: string, email: string, password: st
     const result = await response.json();
     createNotification("success", "Login successful! Welcome back.");
     localStorage.setItem("customerId", result.customer.id);
+    localStorage.setItem("basketKey", `${result.customer.firstName}-shopping-list`);
     //TODO: добавить функцию которая проверит AnonymousBasket и при наличии товара добавит его в корзину пользователя
     sessionStorage.removeItem("basketKey");
     return result;
