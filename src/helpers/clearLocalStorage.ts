b7ff0f9bd0b91cdfea2adf3f04438eb2c16401bd
Dmitry Andriyanov <facklak@mail.ru>
2024-06-11 20:11:33 +0300

    fix: added sessionStorage.clear(), and apiInitialization in clearLocalStorage

--- src/helpers/clearLocalStorage.ts
+++ src/helpers/clearLocalStorage.ts
@@ -1,7 +1,12 @@
-export function clearLocalStorage(): void {
+import { apiInitialization } from "../apiRequests/apiInitialization";
+
+export async function clearLocalStorage(): Promise<void> {
   // localStorage.clear();
   localStorage.removeItem("access_token");
   localStorage.removeItem("refresh_token");
   localStorage.removeItem("customerId");
   localStorage.removeItem("basketKey");
+
+  sessionStorage.clear();
+  await apiInitialization();
 }
